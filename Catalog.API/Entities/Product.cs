7edbb236dfde4113d70482161656d2ae3a9f6cb1
huyns <syhuynguyen1@gmail.com>
2023-08-24 09:57:45 +0700

    #catalogservice: add migration

---
+++ Catalog.API/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Category { get; set; }
+        [MaxLength(200)]
+        public string Summary { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [MaxLength(50)]
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Catalog.API/Migrations/20230823202713_initialCatalogMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalog.API.Migrations
+{
+    public partial class initialCatalogMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageFile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}

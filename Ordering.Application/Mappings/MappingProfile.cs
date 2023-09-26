3a280cbd33aa87ede98963430bba2334867f36dd
huyns <syhuynguyen1@gmail.com>
2023-09-26 17:58:14 +0700

    #ordering-microservice: Ordering Application

---
+++ Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Commands.CheckoutOrder;
+using Ordering.Application.Features.Commands.UpdateOrder;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile() 
+        {
+            // Source-->Target
+            CreateMap<Order, OrderVm>().ReverseMap();
+            CreateMap<CheckoutOrderCommand, Order>().ReverseMap();  
+            CreateMap<UpdateOrderCommand, Order>().ReverseMap();  
+        } 
+    }
+}

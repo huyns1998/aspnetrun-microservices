37795de01a1c59b785ee89eeb082b12056883258
huyns <syhuynguyen1@gmail.com>
2023-10-04 16:49:05 +0700

    #Webapp

---
+++ src/WebApps/AspnetRunBasics/Models/BasketItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspnetRunBasics.Models
+{
+    public class BasketItemModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
